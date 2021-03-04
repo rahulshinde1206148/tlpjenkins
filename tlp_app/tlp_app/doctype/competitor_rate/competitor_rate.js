93e29c7bf08cfd7e974c99877935ecd2a722e9a6
Dell <you@example.com>
2021-03-04 14:53:42 +0530

    Added on change event for Amount.

--- tlp_app/tlp_app/doctype/competitor_rate/competitor_rate.js
+++ tlp_app/tlp_app/doctype/competitor_rate/competitor_rate.js
@@ -6,3 +6,12 @@ frappe.ui.form.on('Competitor Rate', {
 
 	// }
 });
+// onchange event calculations of amount on rate and ordered_quantity
+frappe.ui.form.on("Competitor Rate", "rate", function(frm) {
+	frm.set_value("amount", flt(frm.doc.rate) * flt(frm.doc.ordered_quantity));
+	frm.set_df_property("amount", "read_only" , 1)
+  })
+frappe.ui.form.on("Competitor Rate", "ordered_quantity", function(frm) {
+	frm.set_value("amount", flt(frm.doc.rate) * flt(frm.doc.ordered_quantity));
+	frm.set_df_property("amount", "read_only" , 1)
+  })
